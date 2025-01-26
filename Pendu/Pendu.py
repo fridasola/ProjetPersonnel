e4775f2e72805eb1219fd0a78fdd677e49fe439d
Emi <118975094+fridasola@users.noreply.github.com>
2025-01-26 12:57:54 +0100

    Add files via upload

---
+++ Pendu/Pendu.py
@@ -0,0 +1,60 @@
+# Créé par emily, le 30/12/2024 en Python 3.7
+import random
+
+#initialisation
+LettreAdmisible=[chr(l) for l in range(97,123)]
+print("Vous ne pouvez qu'utiliser des lettres miniscules sans accent")
+NbErreur=7
+print("Vous avez le droit de vous tromper que",NbErreur,"à partir de maintenant")
+Gagnant=False
+LettreUtilisees=[]
+
+#Généré le mot à trouver aléatoirement à partir d'un fichier texte
+FichierObjets=open('c:/Users/emily/OneDrive/Documents/Emily/ProjetPerso/Pendu/Objets.txt','r')
+NumObjet=random.randrange(1,int(FichierObjets.readline()))
+for i in range(1,NumObjet):
+    MotATrouver=FichierObjets.readline()
+
+MotATrouver=MotATrouver[:-1]
+Mot='_' * len(MotATrouver)
+FichierObjets.close()
+FichierObjets=open('Objets.txt','a')
+
+#Jeu
+while not Gagnant:
+    l=input("Veuillez entrer une lettre")
+    if l not in LettreAdmisible:
+        l=int(input("Entrer une autre lettre qui soit en minuscule et sans accent"))
+    if l in MotATrouver:
+        for i in range(len(MotATrouver)):
+            if MotATrouver[i]==l:
+                Mot=''.join([l if MotATrouver[i] == l else Mot[i] for i in range(len(MotATrouver))])
+    else:
+        NbErreur-=1
+        LettreUtilisees.append(l)
+        print("Non. Vous avez le droit de vous tromper que",NbErreur,"à partir de maintenant")
+    print(Mot)
+    if NbErreur<0:
+        Gagnant=True
+        print("Vous avez perdu")
+    if Mot==MotATrouver:
+        Gagnant=True
+        print("Bravo vous avez gagné")
+        NouveauMot=input("Pour nous aider à améliorer le jeu veuillez bien entrer le nom d'un objet en minuscule et sans accent")
+        Bon=True
+        for l in NouveauMot:
+            if not l in LettreAdmisible:
+                Bon=False
+        if Bon:
+            FichierObjets.write("\n"+NouveauMot)
+            with open('Objets.txt', 'r') as fichier:
+                lignes = fichier.readlines()
+
+            # Modifier la première ligne avec le nouveau nombre de mots
+            nouveau_nombre = len(lignes)
+            lignes[0] = f"{nouveau_nombre}\n"
+
+            # Réécrire le contenu dans le fichier
+            with open('Objets.txt', 'w') as fichier:
+                fichier.writelines(lignes)
+FichierObjets.close()
